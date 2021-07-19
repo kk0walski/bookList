f9c0b5676c552d2be1d31d82829bbc2cf0dcd989
kk0walski <karol.kowalski95@gmail.com>
2021-07-19 22:09:23 +0200

    Fix date validation

--- forms.py
+++ forms.py
@@ -10,7 +10,7 @@ class BookForm(FlaskForm):
     isbn = StringField('ISBN', validators=[DataRequired()])
     title = StringField('Title', validators=[DataRequired()])
     author = StringField('Author', validators=[DataRequired()])
-    pubDate = DateField("Publication date", format='%Y.%m.%d')
+    pubDate = DateField("Publication date", format='%Y-%m-%d')
     pages = IntegerField("Pages number")
     fronPage = URLField("frontPage", validators=[url()])
     language = StringField("Language")
@@ -32,12 +32,12 @@ class FilterForm(FlaskForm):
     author = StringField("Author")
     language = StringField("language")
     startdate_field = DateField("Date from", validators=[
-                                DateRange(max=date.today())], format='%Y.%m.%d')
+                                DateRange(max=date.today())], format='%Y-%m-%d')
     enddate_field = DateField("Date To", validators=[
-        DateRange(max=date.today())], format='%Y.%m.%d')
+        DateRange(max=date.today())], format='%Y-%m-%d')
     submit = SubmitField("FILTER")
 
-    def validate_enddate_field(self, form, field):
+    def validate_enddate_field(form, field):
         if form.startdate_field.data:
             if field.data < form.startdate_field.data:
                 raise ValidationError(
