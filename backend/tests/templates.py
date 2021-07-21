a8a2ad5c5ed80095986baa1f228e5c81f2fc2800
kk0walski <karol.kowalski95@gmail.com>
2021-07-21 22:06:50 +0200

    Using app context.

--- backend/tests/templates.py
+++ backend/tests/templates.py
@@ -1,7 +1,9 @@
 import os
 import warnings
+import datetime
 from src.app import create_app
 from flask_testing import TestCase
+from src.model import Book, db
 
 PROJECT_PATH, _ = os.path.split(os.path.dirname(os.path.abspath(__file__)))
 DATABASE_PATH = PROJECT_PATH + '/src/'
@@ -45,5 +47,49 @@ class TestTemplates(TestCase):
         response = self.client.get("/api/books")
         self.assertEquals(response.json, {})
 
+    def test_post(self):
+        app = self.create_app()
+        book = dict(title="Don Quixote", author="Miguel de Cervantes", date=datetime.date(2012, 11, 10),
+            isbn="9788320717501",
+            pages=100,
+            language='fr',
+            url="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/don_quixote.jpg"
+            )
+        with app.test_client() as c:
+            response = self.client.post(
+                "/books/add",
+                data = book,
+                follow_redirects=True, 
+                headers = {"Content-Type":"application/x-www-form-urlencoded"}
+            )
+            books = db.session.query(Book).all()
+            self.assertEqual(db.session.query(Book).count(), 1)
+            self.assert_template_used('books.html')
+            self.assertContext('books', books)
+            self.assert200(response)
+
+    def test_post_wrong(self):
+        app = self.create_app()
+        with app.test_client() as c:
+            response = self.client.post(
+                "/books/add",
+                data = dict(title="Don Quixote", author="Miguel de Cervantes", date=datetime.date(2012, 11, 10),
+                        isbn="97883207175ab",
+                        pages=100,
+                        language='fr',
+                        url="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/don_quixote.jpg"
+                        ),
+                follow_redirects=True, 
+                headers = {"Content-Type":"application/x-www-form-urlencoded"}
+            )
+            self.assertEqual(db.session.query(Book).count(), 0)
+            self.assert_template_used('book.html')
+            self.assert200(response)
+            response = self.client.get('/books/')
+            self.assert_template_used('books.html')
+            self.assertContext('books', [])
+            self.assert200(response)
+
+    
     def tearDown(self):
         os.remove(DATABASE_PATH + TEST_DB)
