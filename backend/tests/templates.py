cc2136eabadce205e28de1f6db0864e0d3f08a36
kk0walski <karol.kowalski95@gmail.com>
2021-07-21 11:02:34 +0200

    Test templates.

--- backend/tests/templates.py
+++ backend/tests/templates.py
@@ -19,7 +19,18 @@ class TestNotRenderTemplates(TestCase):
         app.config['WTF_CSRF_ENABLED'] = False
         return app
 
-    def test_assert_mytemplate_used(self):
+    def test_index(self):
         response = self.client.get("/")
-
         self.assert_template_used('index.html')
+
+    def test_books(self):
+        response = self.client.get("/books/")
+        self.assert_template_used('books.html')
+
+    def test_book(self):
+        response = self.client.get("/books/add")
+        self.assert_template_used('book.html')
+
+    def test_import(self):
+        response = self.client.get("/import/")
+        self.assert_template_used('import.html')
