a0744bb074538febb3dc9f9c704ff379ccc944f9
kk0walski <karol.kowalski95@gmail.com>
2021-07-21 14:35:48 +0200

    Refactor database tests.

--- backend/tests/database.py
+++ backend/tests/database.py
@@ -1,39 +1,63 @@
 import os
-import warnings
 import datetime
+import unittest
+import warnings
 from src.app import create_app
-from flask_testing import TestCase
 from src.model import Book, db
+from sqlalchemy.exc import IntegrityError
+
 
 PROJECT_PATH, _ = os.path.split(os.path.dirname(os.path.abspath(__file__)))
 DATABASE_PATH = PROJECT_PATH + '/src/'
 TEST_DB = "test.db"
 
 
-class TestDatabase(TestCase):
+class TestForms(unittest.TestCase):
 
-    render_templates = True
-
-    def create_app(self):
-
+    def setUp(self):
         warnings.simplefilter('ignore', category=DeprecationWarning)
-        app = create_app(
+        self.app = create_app(
             {"SQLALCHEMY_DATABASE_URI": f"sqlite:///{TEST_DB}", "TESTING": True}
         )
-        app.config['WTF_CSRF_ENABLED'] = False
-        return app
+        self.app.config['WTF_CSRF_ENABLED'] = False
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
 
-    def test_database(self):
+    def test_database_key(self):
         book = Book(title="Don Quixote", author="Miguel de Cervantes", date=datetime.date(2012, 11, 10),
                     isbn="9788320717501",
                     pages=100,
-                    language='polish',
+                    language='fr',
                     url="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/don_quixote.jpg"
                     )
         db.session.add(book)
         db.session.commit()
 
-        assert db.session.query(Book).count() == 1
+        self.assertEqual(db.session.query(Book).count(), 1)
+
+    def test_two_same_isbns(self):
+        book = Book(title="Don Quixote", author="Miguel de Cervantes", date=datetime.date(2012, 11, 10),
+                    isbn="9788320717501",
+                    pages=100,
+                    language='fr',
+                    url="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/don_quixote.jpg"
+                    )
+        db.session.add(book)
+        db.session.commit()
+
+        self.assertEqual(db.session.query(Book).count(), 1)
+
+        with self.assertRaises(IntegrityError):
+            book = Book(title="As I Lay Dying", author="William Faulkner", date=datetime.date(2012, 5, 10),
+                        isbn="9788320717501",
+                        pages=100,
+                        language='en',
+                        url="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/as_I_lay.jpg"
+                        )
+
+            db.session.add(book)
+            db.session.commit()
 
     def tearDown(self):
         os.remove(DATABASE_PATH + TEST_DB)
